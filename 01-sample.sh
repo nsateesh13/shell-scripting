4d876be006b3330e1a9a10468df0f72ea8df520c
Sateesh <nsateesh13@gmail.com>
2022-03-17 00:00:15 +0800

    added new colors

--- 01-sample.sh
+++ 01-sample.sh
@@ -7,5 +7,5 @@ echo $SHELL
 echo -e "\e[31mText in Red Color\e[0m"
 echo "Added one more for colorings ended"
 
-echo -e "\e[32Text in some color\e[0m"
+echo -e "\e[32mText in some color\e[0m"
 echo "added new color"
